d727c5762af88b2438ed071b41672f07d3e5bd99
yan-rds <yyan1207@gmail.com>
2021-11-26 13:46:12 -0300

    Criar CatalogoController

---
+++ src/main/java/br/com/zup/projetoModulo05/catalogo/CatalogoController.java
@@ -0,0 +1,10 @@
+package br.com.zup.projetoModulo05.catalogo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Catalogo")
+public class CatalogoController {
+
+}
