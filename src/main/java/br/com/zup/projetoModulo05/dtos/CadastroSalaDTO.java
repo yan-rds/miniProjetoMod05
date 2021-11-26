adb80262174bfa4ef72afb76a972b5833c77188e
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-11-26 15:23:56 -0300

    Criar DTO CadastroSalaDTO

--- src/main/java/br/com/zup/projetoModulo05/dtos/CadastroSalaDTO.java
+++ src/main/java/br/com/zup/projetoModulo05/dtos/CadastroSalaDTO.java
@@ -1,5 +1,20 @@
 package br.com.zup.projetoModulo05.dtos;
 
-public class EntradaReservaDTO {
+import br.com.zup.projetoModulo05.reservaPoltrona.Reserva;
 
+import java.util.List;
+
+public class CadastroSalaDTO {
+    private List<Reserva> assentos;
+
+    public CadastroSalaDTO() {
+    }
+
+    public List<Reserva> getAssentos() {
+        return assentos;
+    }
+
+    public void setAssentos(List<Reserva> assentos) {
+        this.assentos = assentos;
+    }
 }
