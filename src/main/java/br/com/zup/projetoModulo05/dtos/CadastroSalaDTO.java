7274261120d67e8c481aad604cd81b9bb7959601
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-11-26 17:50:37 -0300

    Atualizar getters e setters da CadastroSalaDTO

--- src/main/java/br/com/zup/projetoModulo05/dtos/CadastroSalaDTO.java
+++ src/main/java/br/com/zup/projetoModulo05/dtos/CadastroSalaDTO.java
@@ -1,20 +1,20 @@
 package br.com.zup.projetoModulo05.dtos;
 
-import br.com.zup.projetoModulo05.reservaPoltrona.Reserva;
+import br.com.zup.projetoModulo05.reservaPoltrona.Assento;
 
 import java.util.List;
 
 public class CadastroSalaDTO {
-    private List<Reserva> assentos;
+    private List<Assento> assentos;
 
     public CadastroSalaDTO() {
     }
 
-    public List<Reserva> getAssentos() {
+    public List<Assento> getAssentos() {
         return assentos;
     }
 
-    public void setAssentos(List<Reserva> assentos) {
+    public void setAssentos(List<Assento> assentos) {
         this.assentos = assentos;
     }
 }
