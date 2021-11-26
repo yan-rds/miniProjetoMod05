f0527ccc62a6fd59232b6fe89b030b711456db38
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-11-26 15:52:40 -0300

    Criar método na Service para cadastrar a lista de assentos na repository

--- src/main/java/br/com/zup/projetoModulo05/reservaPoltrona/ReservaService.java
+++ src/main/java/br/com/zup/projetoModulo05/reservaPoltrona/ReservaService.java
@@ -1,7 +1,21 @@
 package br.com.zup.projetoModulo05.reservaPoltrona;
 
+import br.com.zup.projetoModulo05.sala.Sala;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReservaService {
+
+    @Autowired
+    ReservaRepository reservaRepository;
+
+    /*Receber uma sala da controller, percorrer na lista de Sala todos os assentos e então cadastrar
+    a sala recebida na repository */
+    public void cadastrarAssento(Sala sala){
+        for (Reserva reservaReferencia: sala.getAssentos()) {
+           reservaRepository.save(reservaReferencia);
+        }
+    }
+
 }
