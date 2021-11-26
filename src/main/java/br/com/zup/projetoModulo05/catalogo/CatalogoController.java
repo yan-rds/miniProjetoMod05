f83634a9c6fddb0db5f75b1702ac97fe5488c2ff
yan-rds <yyan1207@gmail.com>
2021-11-26 17:37:33 -0300

    Criar método getCatalogo na Controller

--- src/main/java/br/com/zup/projetoModulo05/catalogo/CatalogoController.java
+++ src/main/java/br/com/zup/projetoModulo05/catalogo/CatalogoController.java
@@ -1,10 +1,19 @@
 package br.com.zup.projetoModulo05.catalogo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/Catalogo")
+@RequestMapping("/catalogo")
 public class CatalogoController {
 
+    @Autowired
+    CatalogoService service;
+
+    @GetMapping
+    public Catalogo getCatalogo(){
+        return service.getCatalogo();
+    }
 }
