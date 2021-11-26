21ae94d15ba736fc563aa50aaa17ee6eb2843eff
yan-rds <yyan1207@gmail.com>
2021-11-26 12:59:48 -0300

    Criar Model Filme

---
+++ src/main/java/br/com/zup/projetoModulo05/catalogo/Filme.java
@@ -0,0 +1,92 @@
+package br.com.zup.projetoModulo05.catalogo;
+
+import java.util.Date;
+import java.util.List;
+
+public class Filme {
+    private String title;
+    private List<Integer> genre_ids;
+    private String overview;
+    private String poster_path;
+    private Date releaseDate;
+    private boolean adult;
+    private String media_type;
+    private double popularity;
+    private double voteAverage;
+
+
+    public Filme() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Integer> getGenre_ids() {
+        return genre_ids;
+    }
+
+    public void setGenre_ids(List<Integer> genre_ids) {
+        this.genre_ids = genre_ids;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPoster_path() {
+        return poster_path;
+    }
+
+    public void setPoster_path(String poster_path) {
+        this.poster_path = poster_path;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public boolean isAdult() {
+        return adult;
+    }
+
+    public void setAdult(boolean adult) {
+        this.adult = adult;
+    }
+
+    public String getMedia_type() {
+        return media_type;
+    }
+
+    public void setMedia_type(String media_type) {
+        this.media_type = media_type;
+    }
+
+    public double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(double popularity) {
+        this.popularity = popularity;
+    }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+}
