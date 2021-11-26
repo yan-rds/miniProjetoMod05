a4be230b42c1b512442c043a42190ef79cbd296e
Michelle Kênia <keniamsoaress@gmail.com>
2021-11-26 15:25:51 -0300

    Criar metodo para listar assentos livres

--- src/main/java/br/com/zup/projetoModulo05/reservaPoltrona/AssentoController.java
+++ src/main/java/br/com/zup/projetoModulo05/reservaPoltrona/AssentoController.java
@@ -1,10 +1,16 @@
 package br.com.zup.projetoModulo05.reservaPoltrona;
 
+import br.com.zup.projetoModulo05.dtos.AssentoDTO;
 import br.com.zup.projetoModulo05.sala.Sala;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/cinema")
@@ -13,9 +19,27 @@ public class AssentoController {
     @Autowired
     AssentoService assentoService;
 
+    @Autowired
+    ModelMapper modelMapper;
+
     @PostMapping("/assentos")
     public void cadastrarSala(Sala novaSala){
         assentoService.cadastrarAssento(novaSala);
+    }
+
+    @GetMapping
+    public List<AssentoDTO> buscarAssentos() {
+
+        List <AssentoDTO> assentosDTOS = new ArrayList<>();
+
+        for (Assento assento: assentoService.exibirAssentosLivres()) {
+
+            AssentoDTO assentoDTO = modelMapper.map(assento, AssentoDTO.class);
+
+            assentosDTOS.add(assentoDTO);
+        }
+
+        return assentosDTOS;
     }
 
 }
