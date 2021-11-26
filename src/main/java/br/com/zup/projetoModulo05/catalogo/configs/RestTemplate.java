5e8039e30e75256615f841f04a493ca2eb625cb2
yan-rds <yyan1207@gmail.com>
2021-11-26 13:18:42 -0300

    Criar Configuration bean para RestTemplate

--- src/main/java/br/com/zup/projetoModulo05/catalogo/configs/RestTemplate.java
+++ src/main/java/br/com/zup/projetoModulo05/catalogo/configs/RestTemplate.java
@@ -1,2 +1,13 @@
-package br.com.zup.projetoModulo05.catalogo.configs;public class RestTemplate {
+package br.com.zup.projetoModulo05.catalogo.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RestTemplate {
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
 }
