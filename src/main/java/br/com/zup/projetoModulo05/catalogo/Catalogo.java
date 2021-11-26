1b4bd5a9420b70b2ebf998c530ecade0fec7b09d
yan-rds <yyan1207@gmail.com>
2021-11-26 17:36:56 -0300

    Atualizar model Catalogo

--- src/main/java/br/com/zup/projetoModulo05/catalogo/Catalogo.java
+++ src/main/java/br/com/zup/projetoModulo05/catalogo/Catalogo.java
@@ -1,18 +1,22 @@
 package br.com.zup.projetoModulo05.catalogo;
-
 import java.util.List;
 
+// Essa Model é o Catálogo exibido pro usuário, tudo que ela possui é uma lista de Title (filmes)
 public class Catalogo {
-    private List<Filme> listaDeFilmes;
+
+
+    private List<Filme> results;
+
 
     public Catalogo() {
     }
 
-    public List<Filme> getListaDeFilmes() {
-        return listaDeFilmes;
+
+    public List<Filme> getResults() {
+        return results;
     }
 
-    public void setListaDeFilmes(List<Filme> listaDeFilmes) {
-        this.listaDeFilmes = listaDeFilmes;
+    public void setResults(List<Filme> results) {
+        this.results = results;
     }
 }
