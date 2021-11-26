12e7af848d534ad4f5bf88c00a7e534d0446e9c0
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-11-26 15:51:30 -0300

    Criar método na Controller para receber uma lista de assentos para serem cadastrados

--- src/main/java/br/com/zup/projetoModulo05/reservaPoltrona/ReservaController.java
+++ src/main/java/br/com/zup/projetoModulo05/reservaPoltrona/ReservaController.java
@@ -1,6 +1,9 @@
 package br.com.zup.projetoModulo05.reservaPoltrona;
 
+import br.com.zup.projetoModulo05.dtos.CadastroSalaDTO;
+import br.com.zup.projetoModulo05.sala.Sala;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,4 +11,10 @@ public class ReservaController {
 
     @Autowired
     ReservaService reservaService;
+
+    @PostMapping
+    public void cadastrarSala(Sala novaSala){
+        reservaService.cadastrarAssento(novaSala);
+    }
+
 }
