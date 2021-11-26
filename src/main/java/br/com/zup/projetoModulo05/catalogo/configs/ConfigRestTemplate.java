af86acc2241be8e80deed4d235e1a57774fce050
yan-rds <yyan1207@gmail.com>
2021-11-26 17:39:10 -0300

    Criar método config @Bean  getRestTemplate

--- src/main/java/br/com/zup/projetoModulo05/catalogo/configs/ConfigRestTemplate.java
+++ src/main/java/br/com/zup/projetoModulo05/catalogo/configs/ConfigRestTemplate.java
@@ -2,10 +2,11 @@ package br.com.zup.projetoModulo05.catalogo.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class RestTemplate {
-
+public class ConfigRestTemplate {
+    // Este método com @Bean é necessário para o Spring gerenciar o RestTemplate (sem o bean ele só ignora)
     @Bean
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
