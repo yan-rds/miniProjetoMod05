bb42460e0298050b81b9bcd7f8961b6f928ce713
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-11-29 10:44:01 -0300

    Criar DTO para Status do Assento

---
+++ src/main/java/br/com/zup/projetoModulo05/dtos/StatusAssentoDTO.java
@@ -0,0 +1,17 @@
+package br.com.zup.projetoModulo05.dtos;
+
+public class StatusAssentoDTO {
+    private boolean estaReservada;
+
+    public StatusAssentoDTO() {
+    }
+
+    public boolean isEstaReservada() {
+        return estaReservada;
+    }
+
+    public void setEstaReservada(boolean estaReservada) {
+        this.estaReservada = estaReservada;
+    }
+
+}
