79e98ab18a55649d6ecf6de87ed5516a127c155a
Michelle Kênia <keniamsoaress@gmail.com>
2021-11-30 15:36:38 -0300

    Criar classe MensagemDeErro

--- src/main/java/br/com/zup/projetoModulo05/config/MensagemDeErro.java
+++ src/main/java/br/com/zup/projetoModulo05/config/MensagemDeErro.java
@@ -1,2 +1,17 @@
-package br.com.zup.projetoModulo05.config;public class MensagemDeErro {
+package br.com.zup.projetoModulo05.config;
+
+public class MensagemDeErro {
+
+    private String mensagemDeErro;
+
+    public MensagemDeErro(String defaultMessage) {
+    }
+
+    public String getMensagemDeErro() {
+        return mensagemDeErro;
+    }
+
+    public void setMensagemDeErro(String mensagemDeErro) {
+        this.mensagemDeErro = mensagemDeErro;
+    }
 }
