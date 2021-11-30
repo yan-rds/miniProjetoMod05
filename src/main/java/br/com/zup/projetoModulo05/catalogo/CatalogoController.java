e13b8b9d5c51997b6910a3b104b934b3b66778cb
yan-rds <yyan1207@gmail.com>
2021-11-30 17:21:18 -0300

    Renomear método getCatalogo para getFilmeEspecifico

--- src/main/java/br/com/zup/projetoModulo05/catalogo/CatalogoController.java
+++ src/main/java/br/com/zup/projetoModulo05/catalogo/CatalogoController.java
@@ -3,8 +3,6 @@ package br.com.zup.projetoModulo05.catalogo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @RestController
 @RequestMapping("/catalogo")
 public class CatalogoController {
@@ -13,9 +11,11 @@ public class CatalogoController {
     CatalogoService service;
 
     @GetMapping("/{query}/{regiao}/{anoDeLancamento}")
-    public Catalogo getCatalogo(@PathVariable String query,
-                                @PathVariable String regiao,
-                                @PathVariable String anoDeLancamento ){
-        return service.getCatalogo(query, regiao, anoDeLancamento);
+    public Catalogo getFilmeEspecifico(@PathVariable String query,
+                                       @PathVariable String regiao,
+                                       @PathVariable String anoDeLancamento){
+        return service.getFilmeEspecifico(query, regiao, anoDeLancamento);
     }
+
+
 }
