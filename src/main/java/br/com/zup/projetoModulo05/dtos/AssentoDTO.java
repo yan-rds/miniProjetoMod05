b881ba6bf79e89af39c3f85ede9dbb1b45d2559c
Michelle Kênia <keniamsoaress@gmail.com>
2021-11-30 12:28:08 -0300

    Atualizar assento DTO com novos atributos

--- src/main/java/br/com/zup/projetoModulo05/dtos/AssentoDTO.java
+++ src/main/java/br/com/zup/projetoModulo05/dtos/AssentoDTO.java
@@ -3,9 +3,18 @@ package br.com.zup.projetoModulo05.dtos;
 public class AssentoDTO {
 
     private int numero;
-    private boolean estaReservada;
+    private boolean assentoLivre;
+    private boolean assentoPreferencial;
 
     public AssentoDTO() {
+    }
+
+    public boolean isAssentoPreferencial() {
+        return assentoPreferencial;
+    }
+
+    public void setAssentoPreferencial(boolean assentoPreferencial) {
+        this.assentoPreferencial = assentoPreferencial;
     }
 
     public int getNumero() {
@@ -16,11 +25,11 @@ public class AssentoDTO {
         this.numero = numero;
     }
 
-    public boolean isEstaReservada() {
-        return estaReservada;
+    public boolean isAssentoLivre() {
+        return assentoLivre;
     }
 
-    public void setEstaReservada(boolean estaReservada) {
-        this.estaReservada = estaReservada;
+    public void setAssentoLivre(boolean assentoLivre) {
+        this.assentoLivre = assentoLivre;
     }
 }
