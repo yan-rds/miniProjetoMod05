eab7e7eb96e7e72496cdad51724189aa67cb2976
Michelle Kênia <keniamsoaress@gmail.com>
2021-11-30 11:38:03 -0300

    Criar lista para filtro assento livre na Repository

--- src/main/java/br/com/zup/projetoModulo05/reservaPoltrona/AssentoRepository.java
+++ src/main/java/br/com/zup/projetoModulo05/reservaPoltrona/AssentoRepository.java
@@ -2,6 +2,10 @@ package br.com.zup.projetoModulo05.reservaPoltrona;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface AssentoRepository extends CrudRepository<Assento, Integer> {
+
+    List <Assento> findAllByAssentoLivre (Boolean assentoLivre);
 
 }
