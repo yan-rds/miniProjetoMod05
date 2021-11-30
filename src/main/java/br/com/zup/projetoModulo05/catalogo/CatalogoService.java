a398faeda20ced3088f127fc20f06de9cea11b8c
yan-rds <yyan1207@gmail.com>
2021-11-30 17:22:19 -0300

    Criar método descobrirFilme

--- src/main/java/br/com/zup/projetoModulo05/catalogo/CatalogoService.java
+++ src/main/java/br/com/zup/projetoModulo05/catalogo/CatalogoService.java
@@ -20,7 +20,7 @@ public class CatalogoService {
 
     // Este método retorna o catalogo de filmes pro usuário
     // A forma que esse catálogo é preenchido eu explico abaixo
-    public Catalogo getCatalogo(String query, String regiao, String anoDeLancamento){
+    public Catalogo getFilmeEspecifico(String query, String regiao, String anoDeLancamento){
 
         /* O UriComponentsBuilder constrói uma URI pra gente, e através dos métodos queryParam eu consigo dar
         chave e valor para os filtros do usuário, esses valores eu tô pegando das variáveis acima */
@@ -37,6 +37,35 @@ public class CatalogoService {
                 .queryParam("primary_release_year", anoDeLancamento)
                 .build();
 
+
+        return template.getForObject(uri.toUriString(), Catalogo.class);
+    }
+
+    public Catalogo descobrirFilme (){
+        /*https://api.themoviedb.org/3/discover/movie?
+        api_key=988decebbe8940b276f0df16a49d8905
+        &language=pt-BR
+        &sort_by=popularity.desc
+        &include_adult=false
+        &include_video=false
+        &page=1
+        &primary_release_year=2021
+        &with_cast=angelina
+        &with_genres=35
+        &with_keywords=venom
+        &with_watch_monetization_types=flatrate
+         */
+
+        UriComponents uri = UriComponentsBuilder.newInstance()
+                .scheme("https")
+                .host("api.themoviedb.org")
+                .path("3/discover/movie")
+                .queryParam("api_key", "988decebbe8940b276f0df16a49d8905")
+                .queryParam("language", "pt-BR")
+                .queryParam("sort_by", "popularity.desc")
+                .queryParam("include_adult", "false")
+                .queryParam("include_video", "false")
+                .build();
 
         return template.getForObject(uri.toUriString(), Catalogo.class);
     }
