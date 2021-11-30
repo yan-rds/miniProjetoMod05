5f6df30d0c13e71d73060490ce9c0d0a8b2cdbc8
yan-rds <yyan1207@gmail.com>
2021-11-30 18:32:01 -0300

    Criar classe MensagemDeErro

--- src/main/java/br/com/zup/projetoModulo05/config/MensagemDeErro.java
+++ src/main/java/br/com/zup/projetoModulo05/config/MensagemDeErro.java
@@ -1,2 +1,19 @@
-package br.com.zup.projetoModulo05.config;public class MensagemDeErro {
+package br.com.zup.projetoModulo05.config;
+
+public class MensagemDeErro {
+
+    private String mensagemDeErro;
+
+    public MensagemDeErro(String mensagemDeErro) {
+        this.mensagemDeErro = mensagemDeErro;
+    }
+
+    public String getMensagemDeErro() {
+        return mensagemDeErro;
+    }
+
+    public void setMensagemDeErro(String mensagemDeErro) {
+        this.mensagemDeErro = mensagemDeErro;
+    }
+
 }
