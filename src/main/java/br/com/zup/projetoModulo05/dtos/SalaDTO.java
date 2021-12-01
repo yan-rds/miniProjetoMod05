7876ec85bc1b215566a971cd444564dd1b7ba072
Michelle Kênia <keniamsoaress@gmail.com>
2021-12-01 17:17:35 -0300

    Criar DTO da model Sala

--- src/main/java/br/com/zup/projetoModulo05/dtos/SalaDTO.java
+++ src/main/java/br/com/zup/projetoModulo05/dtos/SalaDTO.java
@@ -1,2 +1,39 @@
-package br.com.zup.projetoModulo05.dtos;public class SalaDTO {
+package br.com.zup.projetoModulo05.dtos;
+
+import br.com.zup.projetoModulo05.reservaPoltrona.Assento;
+
+import java.util.List;
+
+public class SalaDTO {
+
+    private int id;
+    private List<Assento> assentos;
+    private String nomeFilme;
+
+    public SalaDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Assento> getAssentos() {
+        return assentos;
+    }
+
+    public void setAssentos(List<Assento> assentos) {
+        this.assentos = assentos;
+    }
+
+    public String getNomeFilme() {
+        return nomeFilme;
+    }
+
+    public void setNomeFilme(String nomeFilme) {
+        this.nomeFilme = nomeFilme;
+    }
 }
