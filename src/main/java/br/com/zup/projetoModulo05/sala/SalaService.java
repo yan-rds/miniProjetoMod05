ae5753a6a5e411dfefcfc1fea90439941b960fe9
Michelle Kênia <keniamsoaress@gmail.com>
2021-12-01 18:53:20 -0300

    Criar metodo para cadastrar sala

--- src/main/java/br/com/zup/projetoModulo05/sala/SalaService.java
+++ src/main/java/br/com/zup/projetoModulo05/sala/SalaService.java
@@ -1,2 +1,16 @@
-package br.com.zup.projetoModulo05.sala;public class SalaService {
+package br.com.zup.projetoModulo05.sala;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SalaService {
+
+    @Autowired
+    SalaRepository salaRepository;
+
+    public void cadastrarSala(Sala sala) {
+        salaRepository.save(sala);
+
+    }
 }
