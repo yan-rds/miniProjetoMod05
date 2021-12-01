31ef1ed14c2d3ac73be0964ebe180cb30470b008
yan-rds <yyan1207@gmail.com>
2021-12-01 12:08:30 -0300

    Criar Classe de exceção AlteracaoDisponibilidadeInvalida

---
+++ src/main/java/br/com/zup/projetoModulo05/config/exceptions/AlteracaoDisponibilidadeInvalida.java
@@ -0,0 +1,8 @@
+package br.com.zup.projetoModulo05.config.exceptions;
+
+public class AlteracaoDisponibilidadeInvalida extends RuntimeException{
+
+    public AlteracaoDisponibilidadeInvalida(String message) {
+        super(message);
+    }
+}
