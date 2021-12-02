ae0784b31655ddbe4502992effcac6ca05465654
Michelle Kênia <keniamsoaress@gmail.com>
2021-12-02 14:37:31 -0300

    Criar model horario

--- src/main/java/br/com/zup/projetoModulo05/sala/Horario.java
+++ src/main/java/br/com/zup/projetoModulo05/sala/Horario.java
@@ -1,2 +1,22 @@
-package br.com.zup.projetoModulo05.sala;public class Horario {
+package br.com.zup.projetoModulo05.sala;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Horarios")
+public class Horario {
+
+    @Id
+    private String horarioDaSessao;
+
+    public Horario() {
+    }
+
+    public String getHorarioDaSessao() {
+        return horarioDaSessao;
+    }
+
+    public void setHorarioDaSessao(String horarioDaSessao) {
+        this.horarioDaSessao = horarioDaSessao;
+    }
 }
