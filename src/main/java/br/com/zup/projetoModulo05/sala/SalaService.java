174aa7e8881b2d62d32daa6fe12fd10af390c11d
Michelle Kênia <keniamsoaress@gmail.com>
2021-12-02 17:00:46 -0300

    Criar metodo de exibir salas

--- src/main/java/br/com/zup/projetoModulo05/sala/SalaService.java
+++ src/main/java/br/com/zup/projetoModulo05/sala/SalaService.java
@@ -3,6 +3,8 @@ package br.com.zup.projetoModulo05.sala;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class SalaService {
 
@@ -13,4 +15,14 @@ public class SalaService {
         salaRepository.save(sala);
 
     }
+
+    public List <Sala> exibirTodasAsSalas (String nomeFilme) {
+        if (nomeFilme!=null) {
+            return salaRepository.findAllByNomeFilme(nomeFilme);
+        }
+        List<Sala> salas = (List<Sala>) salaRepository.findAll();
+        return salas;
+
+    }
+
 }
