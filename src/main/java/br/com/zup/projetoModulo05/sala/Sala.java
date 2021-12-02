47c61067ab433115abff14da03747b86eb8b07df
Michelle Kênia <keniamsoaress@gmail.com>
2021-12-02 15:43:37 -0300

    Atualizar atributos de horarios

--- src/main/java/br/com/zup/projetoModulo05/sala/Sala.java
+++ src/main/java/br/com/zup/projetoModulo05/sala/Sala.java
@@ -1,10 +1,10 @@
 package br.com.zup.projetoModulo05.sala;
 
-import br.com.zup.projetoModulo05.catalogo.Filme;
 import br.com.zup.projetoModulo05.reservaPoltrona.Assento;
 
 import javax.persistence.*;
 import javax.validation.Valid;
+import java.sql.Time;
 import java.util.List;
 
 @Entity
@@ -18,8 +18,17 @@ public class Sala {
     @OneToMany(cascade = CascadeType.ALL)
     private List<Assento> assentos;
     private String nomeFilme;
-    @OneToMany(cascade = CascadeType.ALL)
-    private List <Horario> horarios;
+    private Time primeiroHorario;
+    private Time segundoHorario;
+    private Time terceiroHorario;
+    private Time quartoHorario;
+    private Time quintoHorario;
+    private Time sextoHorario;
+    private Time setimoHorario;
+    private Time oitavoHorario;
+    private Time nonoHorario;
+    private Time decimoHorario;
+
     private int numeroAssentos;
 
 
@@ -50,14 +59,6 @@ public class Sala {
         this.nomeFilme = nomeFilme;
     }
 
-    public List<Horario> getHorarios() {
-        return horarios;
-    }
-
-    public void setHorarios(List<Horario> horarios) {
-        this.horarios = horarios;
-    }
-
     public int getNumeroAssentos() {
         return numeroAssentos;
     }
@@ -66,4 +67,83 @@ public class Sala {
         this.numeroAssentos = numeroAssentos;
     }
 
+    public Time getPrimeiroHorario() {
+        return primeiroHorario;
+    }
+
+    public void setPrimeiroHorario(Time primeiroHorario) {
+        this.primeiroHorario = primeiroHorario;
+    }
+
+    public Time getSegundoHorario() {
+        return segundoHorario;
+    }
+
+    public void setSegundoHorario(Time segundoHorario) {
+        this.segundoHorario = segundoHorario;
+    }
+
+    public Time getTerceiroHorario() {
+        return terceiroHorario;
+    }
+
+    public void setTerceiroHorario(Time terceiroHorario) {
+        this.terceiroHorario = terceiroHorario;
+    }
+
+    public Time getQuartoHorario() {
+        return quartoHorario;
+    }
+
+    public void setQuartoHorario(Time quartoHorario) {
+        this.quartoHorario = quartoHorario;
+    }
+
+    public Time getQuintoHorario() {
+        return quintoHorario;
+    }
+
+    public void setQuintoHorario(Time quintoHorario) {
+        this.quintoHorario = quintoHorario;
+    }
+
+    public Time getSextoHorario() {
+        return sextoHorario;
+    }
+
+    public void setSextoHorario(Time sextoHorario) {
+        this.sextoHorario = sextoHorario;
+    }
+
+    public Time getSetimoHorario() {
+        return setimoHorario;
+    }
+
+    public void setSetimoHorario(Time setimoHorario) {
+        this.setimoHorario = setimoHorario;
+    }
+
+    public Time getOitavoHorario() {
+        return oitavoHorario;
+    }
+
+    public void setOitavoHorario(Time oitavoHorario) {
+        this.oitavoHorario = oitavoHorario;
+    }
+
+    public Time getNonoHorario() {
+        return nonoHorario;
+    }
+
+    public void setNonoHorario(Time nonoHorario) {
+        this.nonoHorario = nonoHorario;
+    }
+
+    public Time getDecimoHorario() {
+        return decimoHorario;
+    }
+
+    public void setDecimoHorario(Time decimoHorario) {
+        this.decimoHorario = decimoHorario;
+    }
 }
