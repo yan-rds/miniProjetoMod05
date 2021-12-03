c5be20ef67e28b489748b427ba393796b34fb8cc
yan-rds <yyan1207@gmail.com>
2021-12-03 11:42:57 -0300

    Mover ConfigRestTemplate para o pacote de Config

--- src/main/java/br/com/zup/projetoModulo05/config/ConfigRestTemplate.java
+++ src/main/java/br/com/zup/projetoModulo05/config/ConfigRestTemplate.java
@@ -1,4 +1,4 @@
-package br.com.zup.projetoModulo05.catalogo.configs;
+package br.com.zup.projetoModulo05.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
